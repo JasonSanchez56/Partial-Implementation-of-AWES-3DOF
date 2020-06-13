51e94abce7dfe95287b4779965e61bb9e126fc4b
JasonSanchez56 <52678687+JasonSanchez56@users.noreply.github.com>
2020-06-13 18:21:48 -0500

    Add files via upload

---
+++ Final_types.h
@@ -0,0 +1,34 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: Final_types.h
+ *
+ * Code generated for Simulink model 'Final'.
+ *
+ * Model version                  : 1.180
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Wed May 13 10:38:00 2020
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_Final_types_h_
+#define RTW_HEADER_Final_types_h_
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_Final_T RT_MODEL_Final_T;
+
+#endif                                 /* RTW_HEADER_Final_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
